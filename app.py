b42091a66add2a30826e6fe3c3a4ec3a74e07fa6
NIKHIL2263 <128809717+NIKHIL2263@users.noreply.github.com>
2024-04-17 08:02:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 import streamlit as st
 def foo(a):
     #return a+b
-    a=max(a)
-    return ("Max is  "+a)
+    b=max(a)
+    return ("Max is  ",b)
 st.title("MaxOfThree")
 #a=st.text_input("input your name")
 
 l=st.number_input("input 3 comma seperated numbers")
 #b2=st.number_input("input b")
 #b3=st.number_input("input b")
+print(l)
 
 
 
